0c7f5924d601dd3ebdb81b656a3f3f09fdd8c965
shrestho12 <talimul12bari@gmail.com>
2025-05-03 13:27:42 +0600

    ADD: text to textnodes'

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -45,5 +45,11 @@ class TestHTMLNODE(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
+
+
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType , text_node_to_html_node
-from text_splitter import split_nodes_delimiter , extract_markdown_images , extract_markdown_links, split_nodes_image , split_nodes_link
+from text_splitter import split_nodes_delimiter , extract_markdown_images , extract_markdown_links, split_nodes_image , split_nodes_link,text_to_textnodes
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -233,6 +233,55 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_mixed_content(self):
+        input_text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected_output = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        output = text_to_textnodes(input_text)
+        self.assertEqual(output, expected_output)
+
+    def test_plain_text_only(self):
+        self.assertEqual(
+            text_to_textnodes("Just some plain text."),
+            [TextNode("Just some plain text.", TextType.TEXT)]
+        )
+
+    def test_multiple_styles(self):
+        self.assertEqual(
+            text_to_textnodes("**bold** _italic_ `code`"),
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" ", TextType.TEXT),
+                TextNode("code", TextType.CODE),
+            ]
+        )
+
+    def test_link_and_image(self):
+        self.assertEqual(
+            text_to_textnodes("![img](img.jpg) and [click](url.com)"),
+            [
+                TextNode("img", TextType.IMAGE, "img.jpg"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("click", TextType.LINK, "url.com"),
+            ]
+        )
+
+
 
         
 if __name__ == "__main__":
